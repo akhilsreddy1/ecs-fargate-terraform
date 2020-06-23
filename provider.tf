cd2d0fb2f7f9c8017c158b2bc4d7fd490d84a7b7
akhilsreddy1 <47410806+akhilsreddy1@users.noreply.github.com>
2020-06-23 14:09:47 -0500

    Add files via upload

--- provider.tf
+++ provider.tf
@@ -2,7 +2,5 @@
 provider "aws" {
   version                 = "~> 2.43.0"
   region                  = "us-west-2"
-  shared_credentials_file = "/root/.aws/credentials"
-  profile                 = "skylab"
 }
 
